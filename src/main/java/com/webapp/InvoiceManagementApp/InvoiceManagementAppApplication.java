11e43f9caf4fea47b4f430c8551ee33f9d88bd15
kentmarcusp <kent.marcus.p@gmail.com>
2023-02-20 19:52:56 +0200

    Initial commit

---
+++ src/main/java/com/webapp/InvoiceManagementApp/InvoiceManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.InvoiceManagementApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceManagementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webapp/InvoiceManagementApp/InvoiceManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.InvoiceManagementApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
