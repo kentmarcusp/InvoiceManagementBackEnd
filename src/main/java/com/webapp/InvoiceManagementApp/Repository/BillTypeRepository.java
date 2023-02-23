89bc9f15200862543021d6c90e3e48ecfa9a522d
kentmarcusp <kent.marcus.p@gmail.com>
2023-02-23 02:15:18 +0200

    Third commit: added some more entities/specifications + remodeled the main class

--- src/main/java/com/webapp/InvoiceManagementApp/Repository/BillTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.webapp.InvoiceManagementApp.Repository;
-
-import com.webapp.InvoiceManagementApp.Model.BillType;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BillTypeRepository extends JpaRepository<BillType, Long> {
-}
